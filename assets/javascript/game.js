7a1485a46a546609d1c3b829dc365e6c7cf53281
salemgrace <salem.bryant@gmail.com>
2018-08-23 22:26:27 -0400

    pseudo-coded

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// A random number needs generated
+
+// Each gem needs to equal a random number between 1-12
+
+// Each time I click a gem the score needs to go up by that assigned value
+
+// When your score equals the random generated number - win!
+
+// If you go over the random generated number - lose!
+
+// Game reset - new random number, new gem numbers
+
